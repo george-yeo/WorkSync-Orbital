7cdcb9f24821223d9d71cd368974cd38b8f5b2f7
sooyitao <sooyitao@gmail.com>
2024-05-21 20:25:26 +0800

    merge

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-const express = require('express')
-=======
 require('dotenv').config()
 
 const express =  require('express')
->>>>>>> 814f68a093e0fcb7814c2d67d59ea36d181fc307
 
 // express app
 const app = express()
 
-<<<<<<< HEAD
-// listen for requests
-app.listen(4000, () => {
-    console.log('listening on port 4000')
-=======
 // middleware
 app.use(express.json())
 
@@ -30,5 +21,4 @@ app.get('/', (req, res) => {
 // listen for requests
 app.listen(process.env.PORT, () => {
     console.log('listening on port', process.env.PORT)
->>>>>>> 814f68a093e0fcb7814c2d67d59ea36d181fc307
 })
