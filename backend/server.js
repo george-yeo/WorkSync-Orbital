0b15b0d1dcfcdcbdbd5ddd451d218b55c6c984b7
sooyitao <sooyitao@gmail.com>
2024-05-21 20:57:57 +0800

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
-const express =  require('express')
+const express = require('express')
+const taskRoutes = require('./routes/tasks')
 
 // express app
 const app = express()
@@ -9,16 +10,14 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method)
+  next()
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/tasks', taskRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
+  console.log('listening on port', process.env.PORT)
 })
