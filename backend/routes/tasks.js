658da5f92639eee80c009887c57e0ea43bcab4a9
George <156641986+george-yeo@users.noreply.github.com>
2024-05-21 21:22:00 +0800

    INDENTATION

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,36 +3,36 @@ const Task = require('../models/Task')
 
 const router = express.Router()
 
-// GET all workouts
+// GET all tasks
 router.get('/', (req, res) => {
-    res.json({mssg: 'GET all tasks'})
-  })
+  res.json({mssg: 'GET all tasks'})
+})
+
+// GET a single task
+router.get('/:id', (req, res) => {
+  res.json({mssg: 'GET a single task'})
+})
   
-  // GET a single workout
-  router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single task'})
-  })
-  
-  // POST a new workout
-  router.post('/', async (req, res) => {
-    const {title, description, deadline, isCompleted} = req.body
-  
+// POST a new task
+router.post('/', async (req, res) => {
+  const {title, description, deadline, isCompleted} = req.body
+
   try {
     const task = await Task.create({title, description, deadline, isCompleted})
     res.status(200).json(task)
   } catch (error) {
     res.status(400).json({error: error.message})
   }
-  })
+})
   
-  // DELETE a workout
-  router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a workout'})
-  })
+// DELETE a task
+router.delete('/:id', (req, res) => {
+  res.json({mssg: 'DELETE a task'})
+})
   
-  // UPDATE a workout
-  router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a workout'})
-  })
+// UPDATE a task
+router.patch('/:id', (req, res) => {
+  res.json({mssg: 'UPDATE a task'})
+})
   
-  module.exports = router
+module.exports = router
