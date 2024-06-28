668b3409f4249d1f523cb2c811e06a937804f8b4
George <156641986+george-yeo@users.noreply.github.com>
2024-06-28 20:50:40 +0800

    Update Channel.js

--- frontend/src/components/chat-components/Channels.js
+++ frontend/src/components/chat-components/Channels.js
@@ -7,13 +7,21 @@ import Channel from './Channel';
 const Channels = ({ channels, selectChannel }) => {
   const chatContext = useChatContext()
   
+  let channelsList = channels
+  if (chatContext.isRecent) {
+    channelsList = []
+    for(var i = channels.length-1; i >= 0; i--) {
+      channelsList.push(channels[i]);
+    }
+  }
+  
   return (
     <div>
       <h5>{chatContext.isRecent ? "Recent Chats" : "Search Results"}</h5>
       {!chatContext.isRecent && channels.length === 0 && <div>No results found...</div>}
       {chatContext.isRecent && channels.length === 0 && <div>No recent chats available...</div>}
       <ul>
-        {channels && channels.map(channel => (
+        {channels && channelsList.map(channel => (
           <Channel channel={channel} key={channel._id} selectChannel={selectChannel}/>
         ))}
       </ul>
